718efa5cd41b3f5f8ec5446b0f6fc206e69e176a
MRIGANK GUPTA <95197331+yourrandomvariable@users.noreply.github.com>
2023-11-27 03:19:41 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -18,16 +18,16 @@ document.addEventListener('DOMContentLoaded', function () {
         activeLink = navItem;
     };
 
-    // const fragment = window.location.hash;
+    const fragment = window.location.hash;
 
-    // if (fragment) {
-    //     const targetNavItem = document.querySelector(
-    //         `a[href="${fragment}"] li`
-    //     );
-    //     setActiveNavLink(targetNavItem);
-    // }
+    if (fragment) {
+        const targetNavItem = document.querySelector(
+            `a[href="${fragment}"] li`
+        );
+        setActiveNavLink(targetNavItem);
+    }
 
-    // Mobile Menu
+    Mobile Menu
 
     const toggleMenu = () => {
         const menuSelector = document.querySelector('div[aria-label="menu"]');
@@ -55,32 +55,32 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    // On scroll check active section
-    // const sections = document.querySelectorAll('section');
-    // let activeSection = 'hero';
+    On scroll check active section
+    const sections = document.querySelectorAll('section');
+    let activeSection = 'hero';
 
-    // const isSectionInView = (section) => {
-    //     const rect = section.getBoundingClientRect();
-    //     return rect.top >= 0 && rect.top <= 100;
-    // };
+    const isSectionInView = (section) => {
+        const rect = section.getBoundingClientRect();
+        return rect.top >= 0 && rect.top <= 100;
+    };
 
-    // const updateSection = () => {
-    //     sections.forEach((section) => {
-    //         if (isSectionInView(section)) {
-    //             activeSection = section.id;
-    //             console.log(activeSection);
-    //         }
-    //     });
-    // };
+    const updateSection = () => {
+        sections.forEach((section) => {
+            if (isSectionInView(section)) {
+                activeSection = section.id;
+                console.log(activeSection);
+            }
+        });
+    };
 
-    // const handleScroll = () => {
-    //     updateSection();
-    //     activeLink = document.querySelector('.nav-items.active-nav-link');
-    //     const targetNavItem = document.querySelector(
-    //         `a[href="#${activeSection}"] li`
-    //     );
-    //     setActiveNavLink(targetNavItem);
-    // };
+    const handleScroll = () => {
+        updateSection();
+        activeLink = document.querySelector('.nav-items.active-nav-link');
+        const targetNavItem = document.querySelector(
+            `a[href="#${activeSection}"] li`
+        );
+        setActiveNavLink(targetNavItem);
+    };
 
-    // window.addEventListener('scroll', handleScroll);
+    window.addEventListener('scroll', handleScroll);
 });
